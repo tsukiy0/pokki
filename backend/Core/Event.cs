3d0c9753d866041a0a736e18e5e76983bd3f9499
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-16 21:12:33 +1100

    feat: define Event and NewGameEvent

---
+++ backend/Core/Event.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Core
+{
+    public class GameId : Guid { }
+
+    public class EventVersion
+    {
+        private readonly int _value;
+
+        public EventVersion(int value)
+        {
+            _value = value;
+        }
+
+        public readonly int ToInt()
+        {
+            return _value;
+        }
+    }
+
+    public sealed abstract class Event
+    {
+        public readonly GameId GameId;
+        public readonly EventVersion Version;
+
+        public Event(GameId gameId, EventVersion version)
+        {
+            GameId = gameId;
+            Version = version;
+        }
+    }
+
+    public class NewGameEvent : Event
+    {
+        public readonly Person Admin;
+
+        public NewGameEvent(GameId gameId, EventVersion version, Person admin) : base(gameId, version)
+        {
+            Admin = admin;
+        }
+    }
+}
---
+++ backend/Core/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Core
+{
+    public class PersonId : Guid
+    {
+    }
+
+    public class Person
+    {
+        public readonly PersonId Id;
+        public readonly string Name;
+
+        public Person(PersonId id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
