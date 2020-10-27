f8efdd523595d35de742a049977144e6708d50e1
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-27 22:34:53 +1100

    feat: add define all events

--- packages/core/src/Game/Event.ts
+++ packages/core/src/Game/Event.ts
@@ -1,12 +1,16 @@
 import { Comparable } from "@tsukiy0/tscore";
 import { UserId } from "../User/User";
+import { CardId } from "./Card";
 import { CardSet } from "./CardSet";
 import { GameId } from "./Game";
+import { PlayerCard } from "./PlayerCard";
 
 export abstract class Event implements Comparable {
   constructor(public readonly gameId: GameId) {}
 
-  abstract equals(input: this): boolean;
+  equals(input: this): boolean {
+    return this.gameId.equals(input.gameId);
+  }
 }
 
 export class NewGameEvent extends Event {
@@ -20,7 +24,7 @@ export class NewGameEvent extends Event {
 
   equals(input: this): boolean {
     return (
-      this.gameId.equals(input.gameId) &&
+      super.equals(input) &&
       this.adminId.equals(input.adminId) &&
       this.cards.equals(input.cards)
     );
@@ -33,20 +37,28 @@ export class AddPlayerEvent extends Event {
   }
 
   equals(input: this): boolean {
-    return (
-      this.gameId.equals(input.gameId) && this.playerId.equals(input.playerId)
-    );
+    return super.equals(input) && this.playerId.equals(input.playerId);
   }
 }
 
-export class NewRoundEvent extends Event {
-  constructor(gameId: GameId, public readonly roundI) {
+export class NewRoundEvent extends Event {}
+
+export class PlayCardEvent extends Event {
+  constructor(gameId: GameId, public readonly playerCard: PlayerCard) {
     super(gameId);
   }
 
   equals(input: this): boolean {
-    return (
-      this.gameId.equals(input.gameId) && this.playerId.equals(input.playerId)
-    );
+    return super.equals(input) && this.playerCard.equals(this.playerCard);
+  }
+}
+
+export class EndRoundEvent extends Event {
+  constructor(gameId: GameId, public readonly resultCardId: CardId) {
+    super(gameId);
+  }
+
+  equals(input: this): boolean {
+    return super.equals(input) && this.resultCardId.equals(input.resultCardId);
   }
 }
