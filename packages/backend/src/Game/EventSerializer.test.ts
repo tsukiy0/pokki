e55dbec420dd5fd58bf979f95fa6352f60bb2ada
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-29 03:22:02 +1100

    feat: impl all serializers for events

--- packages/backend/src/Game/EventSerializer.test.ts
+++ packages/backend/src/Game/EventSerializer.test.ts
@@ -3,14 +3,21 @@ import {
   Card,
   CardIdRandomizer,
   CardSet,
+  EndRoundEvent,
   GameIdRandomizer,
   NewGameEvent,
+  NewRoundEvent,
+  PlayCardEvent,
+  RoundIdRandomizer,
   UserIdRandomizer,
 } from "@pokki/core";
 import { testSerializer } from "@tsukiy0/tscore/dist/index.testTemplate";
 import {
   AddPlayerEventSerializer,
+  EndRoundEventSerializer,
   NewGameEventSerializer,
+  NewRoundEventSerializer,
+  PlayCardEventSerializer,
 } from "./EventSerializer";
 import { EventVersion } from "./EventVersion";
 
@@ -34,5 +41,42 @@ describe("AddPlayerEventSerializer", () => {
     new AddPlayerEventSerializer(new EventVersion(1)),
     () =>
       new AddPlayerEvent(GameIdRandomizer.random(), UserIdRandomizer.random()),
+  );
+});
+
+describe("NewRoundEvent", () => {
+  testSerializer(
+    new NewRoundEventSerializer(new EventVersion(1)),
+    () =>
+      new NewRoundEvent(
+        GameIdRandomizer.random(),
+        UserIdRandomizer.random(),
+        RoundIdRandomizer.random(),
+        "round1",
+      ),
+  );
+});
+
+describe("PlayCardEvent", () => {
+  testSerializer(
+    new PlayCardEventSerializer(new EventVersion(1)),
+    () =>
+      new PlayCardEvent(
+        GameIdRandomizer.random(),
+        UserIdRandomizer.random(),
+        CardIdRandomizer.random(),
+      ),
+  );
+});
+
+describe("EndRoundEvent", () => {
+  testSerializer(
+    new EndRoundEventSerializer(new EventVersion(1)),
+    () =>
+      new EndRoundEvent(
+        GameIdRandomizer.random(),
+        UserIdRandomizer.random(),
+        CardIdRandomizer.random(),
+      ),
   );
 });
--- packages/backend/src/Game/EventSerializer.ts
+++ packages/backend/src/Game/EventSerializer.ts
@@ -3,8 +3,12 @@ import {
   Card,
   CardId,
   CardSet,
+  EndRoundEvent,
   GameId,
   NewGameEvent,
+  NewRoundEvent,
+  PlayCardEvent,
+  RoundId,
   UserId,
 } from "@pokki/core";
 import { Serializer } from "@tsukiy0/tscore";
@@ -44,12 +48,15 @@ export class NewGameEventSerializer
 
   deserialize(input: AttributeMap): NewGameEvent {
     return new NewGameEvent(
-      new GameId(input.id!.S),
-      new UserId(input.player_id!.S),
+      new GameId(input.id.S as string),
+      new UserId(input.player_id.S as string),
       new CardSet(
-        input.cards!.L.map((item) => {
-          return new Card(new CardId(item!.M.id!.S), item!.M.name!.S);
-        }),
+        input.cards?.L?.map((item) => {
+          return new Card(
+            new CardId(item?.M?.id.S as string),
+            item?.M?.name.S as string,
+          );
+        }) as Card[],
       ),
     );
   }
@@ -70,8 +77,79 @@ export class AddPlayerEventSerializer
 
   deserialize(input: AttributeMap): AddPlayerEvent {
     return new AddPlayerEvent(
-      new GameId(input.id!.S),
-      new UserId(input.player_id!.S),
+      new GameId(input.id.S as string),
+      new UserId(input.player_id.S as string),
+    );
+  }
+}
+
+export class NewRoundEventSerializer
+  implements Serializer<NewRoundEvent, AttributeMap> {
+  constructor(public readonly eventVersion: EventVersion) {}
+
+  serialize(input: NewRoundEvent): AttributeMap {
+    return {
+      id: { S: input.gameId.toString() },
+      version: { N: this.eventVersion.toNumber().toString() },
+      type: { S: EventType.NEW_GAME },
+      player_id: { S: input.playerId.toString() },
+      round_id: { S: input.roundId.toString() },
+      round_name: { S: input.roundName },
+    };
+  }
+
+  deserialize(input: AttributeMap): NewRoundEvent {
+    return new NewRoundEvent(
+      new GameId(input.id.S as string),
+      new UserId(input.player_id.S as string),
+      new RoundId(input.round_id.S as string),
+      input.round_name.S as string,
+    );
+  }
+}
+
+export class PlayCardEventSerializer
+  implements Serializer<PlayCardEvent, AttributeMap> {
+  constructor(public readonly eventVersion: EventVersion) {}
+
+  serialize(input: PlayCardEvent): AttributeMap {
+    return {
+      id: { S: input.gameId.toString() },
+      version: { N: this.eventVersion.toNumber().toString() },
+      type: { S: EventType.NEW_GAME },
+      player_id: { S: input.playerId.toString() },
+      card_id: { S: input.cardId.toString() },
+    };
+  }
+
+  deserialize(input: AttributeMap): PlayCardEvent {
+    return new PlayCardEvent(
+      new GameId(input.id.S as string),
+      new UserId(input.player_id.S as string),
+      new CardId(input.card_id.S as string),
+    );
+  }
+}
+
+export class EndRoundEventSerializer
+  implements Serializer<EndRoundEvent, AttributeMap> {
+  constructor(public readonly eventVersion: EventVersion) {}
+
+  serialize(input: EndRoundEvent): AttributeMap {
+    return {
+      id: { S: input.gameId.toString() },
+      version: { N: this.eventVersion.toNumber().toString() },
+      type: { S: EventType.NEW_GAME },
+      player_id: { S: input.playerId.toString() },
+      result_card_id: { S: input.resultCardId.toString() },
+    };
+  }
+
+  deserialize(input: AttributeMap): EndRoundEvent {
+    return new EndRoundEvent(
+      new GameId(input.id.S as string),
+      new UserId(input.player_id.S as string),
+      new CardId(input.result_card_id.S as string),
     );
   }
 }
