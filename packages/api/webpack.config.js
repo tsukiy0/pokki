f93529e51b34c60ba4bc9b524a0fedc44d23f3e8
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-30 23:26:46 +1100

    feat: build api

---
+++ packages/api/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.join(__dirname, "src/index.ts"),
+  output: {
+    path: path.join(__dirname, "out"),
+    filename: "index.js",
+    library: "index",
+    libraryTarget: "commonjs2",
+  },
+  target: "node",
+  plugins: [
+    new webpack.IgnorePlugin(/pg-native/),
+    new webpack.IgnorePlugin(/mssql/),
+    new webpack.IgnorePlugin(/mysql/),
+    new webpack.IgnorePlugin(/mysql2/),
+    new webpack.IgnorePlugin(/sqlite3/),
+    new webpack.IgnorePlugin(/pg-query-stream/),
+    new webpack.IgnorePlugin(/oracledb/),
+  ],
+  mode: "production",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: "tsconfig.build.json",
+          },
+        },
+      },
+    ],
+  },
+};
